e562e5558233c7d1e1f62b4811162964d3ea13b4
SitaraTrue <sitaratrue@gmail.com>
2026-03-21 19:20:57 -0600

    Split the data

--- Question 4.py
+++ Question 4.py
@@ -1,1 +1,15 @@
 ''' Neural Network for Binary Classification '''
+
+# Load dataset
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+data = load_breast_cancer()
+
+# Construct feature matrix X and target vector y
+features_x = data.data
+target_y = data.target
+
+# Create train-test split
+features_train, features_test, labels_train, labels_test = train_test_split(features_x, target_y, test_size=.2, random_state=19)
