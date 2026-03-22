899d29462148e658658f7b455255593f6d055d8b
SitaraTrue <sitaratrue@gmail.com>
2026-03-21 19:37:04 -0600

    Create neural network model

--- Question 4.py
+++ Question 4.py
@@ -3,19 +3,42 @@
 # Load dataset
 from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, InputLayer
 from sklearn.metrics import accuracy_score
 data = load_breast_cancer()
+tf.random.set_seed(1)
 
 # Construct feature matrix X and target vector y
 features_x = data.data
 target_y = data.target
-
-# Create train-test split
-features_train, features_test, labels_train, labels_test = train_test_split(features_x, target_y, test_size=.2, random_state=19)
 
 # Standardize the data
 mu = features_x.mean(axis=(0,1))
 sigma = features_x.std(axis=(0,1))
 x_std = (features_x-mu) / (sigma + 1e-8)
 
+# Create train-test split
+features_train, features_test, labels_train, labels_test = train_test_split(x_std, target_y, test_size=.2, random_state=19)
+
+# Train neural network
+neural_network_model = Sequential()
+input_layer = InputLayer(input_shape=(30,))
+neural_network_model.add(input_layer)
+hidden_layer=Dense(15)
+neural_network_model.add(hidden_layer)
+output_layer = Dense(1, activation='sigmoid')
+neural_network_model.add(output_layer)
+
+# Compile
+neural_network_model.compile(loss='binary_crossentropy', metrics=['accuracy'])
+
+# Fit
+neural_network_model.fit(x_std, target_y, epochs=10)
+
+# Evaluate
+test_accuracy = neural_network_model.evaluate(features_test, labels_test)
+train_accuracy = neural_network_model.evaluate(features_train, labels_train)
+print(f"Test accuracy: {test_accuracy[1]}")
+print(f"Train accuracy: {train_accuracy[1]}")
