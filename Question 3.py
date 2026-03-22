8a13b0caa804e48abcd2b817e38b63d7da589d69
SitaraTrue <sitaratrue@gmail.com>
2026-03-21 19:12:46 -0600

    Adjust decision tree and predict

--- Question 3.py
+++ Question 3.py
@@ -4,6 +4,7 @@
 from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
 data = load_breast_cancer()
 
 # Construct feature matrix X and target vector y
@@ -14,5 +15,15 @@ target_y = data.target
 features_train, features_test, labels_train, labels_test = train_test_split(features_x, target_y, test_size=.2, random_state=19)
 
 # Create decision tree model
-decision_tree_classifier = DecisionTreeClassifier(criterion='entropy')
+decision_tree_classifier = DecisionTreeClassifier(criterion='entropy', max_depth=8, min_impurity_decrease=0.01)
 decision_tree_classifier.fit(features_train, labels_train)
+
+# Predict
+predicted_labels_test = decision_tree_classifier.predict(features_test)
+test_accuracy = accuracy_score(labels_test, predicted_labels_test)
+
+predicted_labels_train = decision_tree_classifier.predict(features_train)
+train_accuracy = accuracy_score(labels_train, predicted_labels_train)
+
+print(f"Test accuracy: {test_accuracy}")
+print(f"Training accuracy: {train_accuracy}")
