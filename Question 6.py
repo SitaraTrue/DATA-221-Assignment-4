5afa3af3080156d3f4f5fb3dce4ab67fa5a92ffb
SitaraTrue <sitaratrue@gmail.com>
2026-03-21 20:54:00 -0600

    Finish Question 6

--- Question 6.py
+++ Question 6.py
@@ -2,7 +2,6 @@
 
 # Load dataset
 from tensorflow.keras.datasets import fashion_mnist
-import tensorflow as tf
 from tensorflow.keras import layers,models
 (X_train, y_train), (X_test, y_test) = fashion_mnist.load_data()
 
