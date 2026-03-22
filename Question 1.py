ca28bcf782fbdb6ec3941c6499cf104d08612c88
SitaraTrue <sitaratrue@gmail.com>
2026-03-21 18:48:57 -0600

    Finished Q1

--- Question 1.py
+++ Question 1.py
@@ -8,9 +8,24 @@ data = load_breast_cancer()
 
 features_x = data.data
 target_y = data.target
-print(features_x, target_y)
 
-''' Shape of X and y
+# Report on shape of X and y
+print(f"X shape: {features_x.shape}") # (569,30)
+print(f"y shape: {target_y.shape}") # (569,)
 
+# Report the number of samples belonging to each class
+counter0 = 0
+counter1 = 0
+for row in target_y:
+    if row == 0:
+        counter0 += 1
+    else:
+        counter1 += 1
+print(f"Class 0: {counter0} samples")
+print(f"Class 1: {counter1} samples")
 
+''' Balance
+- This dataset is imbalanced, as class 0 takes up about a third and class 1 is two-thirds.
+- Balance is important so that the model has enough information to learn about each class equally.
+    A balanced dataset leads to higher quality predictions.
 '''
