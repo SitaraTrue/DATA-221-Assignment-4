24bfa7637c5eaee17c18f8d8a93c2f7007ed86af
SitaraTrue <sitaratrue@gmail.com>
2026-03-21 18:27:55 -0600

    Construct feature matrix and target vector

--- Question 1.py
+++ Question 1.py
@@ -2,6 +2,15 @@
 
 # Load dataset
 from sklearn.datasets import load_breast_cancer
-data = load_breast_cancer
+data = load_breast_cancer()
 
-# Construct feature matrix and tar
+# Construct feature matrix X and target vector y
+
+features_x = data.data
+target_y = data.target
+print(features_x, target_y)
+
+''' Shape of X and y
+
+
+'''
